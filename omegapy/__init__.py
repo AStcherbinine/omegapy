5fbf698a54eb987eef058687ae4641b26e2cf9cb
aurelien stcherbinine <aurelien.stcherbinine@ias.u-psud.fr>
2020-03-06 22:42:52 +0100

    add __init__.py

---
+++ omegapy/__init__.py
@@ -0,0 +1,13 @@
+'''OMEGA Py :
+
+Submodules
+==========
+omega_data :
+    Importation and correction (thermal + atmospheric) of OMEGA/MEx observations.
+omega_plots :
+    Display of OMEGA/MEx observations.
+useful_functions :
+    Useful python functions, some of them are used in omega_data and omega_plots.
+'''
+
+from . import omega_data, omega_plots, useful_functions
