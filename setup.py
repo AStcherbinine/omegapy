8b9a828b71e12348457e8a8d946c02ccc778629e
Aurélien Stcherbinine <aurelien.stcherbinine@gmail.com>
2022-03-11 19:39:08 -0600

    bump to version 2.2.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ package_data = {
 
 setuptools.setup(
     name='omegapy',
-    version='2.2.7',
+    version='2.2.8',
     author='Aurélien Stcherbinine',
     author_email='aurelien@stcherbinine.net',
     description='Python tools for OMEGA/MEx observations analysis',
