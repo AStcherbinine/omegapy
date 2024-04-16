0bd1c717c562388d75ee43e6439ae65505f8107b
Aurélien Stcherbinine <aurelien.stcherbinine@gmail.com>
2024-04-16 16:39:30 +0200

    change Python version min entry name in JSON file

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(),
     package_data=package_data,
-    python_requires='>='+data_json['python-version-min'],
+    python_requires='>='+data_json['PythonVersionMin'],
     setup_requires=['wheel'],
     install_requires=requirements,
     classifiers=[
